e2feaf1963a7598fba4b7dff04cc072c666aeb3c
mdemon420 <mdeamon4@gmail.com>
2022-11-01 22:50:21 +0600

    exercise 4

--- Day 3 - Beginner - Control Flow and Logical Operators/operator.py
+++ Day 3 - Beginner - Control Flow and Logical Operators/operator.py
@@ -1,27 +1,27 @@
-print("Welcome to the rollercoaster")
-height = int(input("What is your height in cm? :"))
-bill = 0
-if height >= 80:
-    print("You can ride the rollercoaster")
-    age = int(input("What is your age? :"))
-    if age < 12:
-        bill = 5
-        print("Child ticket are $5")
-    elif age <= 18:
-        bill = 7
-        print("Youth ticket are $7")
-    else:
-        bill = 12
-        print("Adult ticket are $12")
-    want_photo = input("Do you want a photo taken? Y or N :")
-    if want_photo == "Y":
-        bill = bill + 3
-        print(f"Your final bill is ${bill}")
-    else:
-        bill
-        print(f"Your final bill is ${bill}")
-else:
-    print("Sorry, you have to grow taller before you can ride")
+# print("Welcome to the rollercoaster")
+# height = int(input("What is your height in cm? :"))
+# bill = 0
+# if height >= 80:
+#     print("You can ride the rollercoaster")
+#     age = int(input("What is your age? :"))
+#     if age < 12:
+#         bill = 5
+#         print("Child ticket are $5")
+#     elif age <= 18:
+#         bill = 7
+#         print("Youth ticket are $7")
+#     else:
+#         bill = 12
+#         print("Adult ticket are $12")
+#     want_photo = input("Do you want a photo taken? Y or N :")
+#     if want_photo == "Y":
+#         bill = bill + 3
+#         print(f"Your final bill is ${bill}")
+#     else:
+#         bill
+#         print(f"Your final bill is ${bill}")
+# else:
+#     print("Sorry, you have to grow taller before you can ride")
 
 
 
@@ -71,3 +71,33 @@ else:
 #         print(f"{year} is a leap year")
 # else:
 #     print(f"{year} is not a leap year")
+
+
+# Exercise -4****************************************************************
+
+print("Welcome to python Pizza Deliveries")
+# 
+# medium_pizza = 20
+# 
+size = input("What size pizza do you want? S,M or L : ")
+add_pepperoni = input("Do you want pepperoni? Y or N : ")
+extra_cheese = input("Do you want extra cheese? Y or N : ")
+
+bill = 0
+
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+else:
+    bill += 25
+
+if add_pepperoni == "Y":
+    bill +=2
+else:
+    bill +=3
+
+if extra_cheese == "Y":
+    bill +=1
+
+print(f"Your final bill is {bill}")
