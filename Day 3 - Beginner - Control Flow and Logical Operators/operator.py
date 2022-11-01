b284a95c232fa1ae53c3b24eef2298252b2c4156
mdemon420 <mdeamon4@gmail.com>
2022-11-01 20:08:27 +0600

    done

--- Day 3 - Beginner - Control Flow and Logical Operators/operator.py
+++ Day 3 - Beginner - Control Flow and Logical Operators/operator.py
@@ -1,17 +1,27 @@
-# print("Welcome to the rollercoaster")
-# height = int(input("What is your height in cm?"))
-
-# if height >= 80:
-#     print("You can ride the rollercoaster")
-#     age = int(input("What is your age?"))
-#     if age < 12:
-#         print("Please pay $5.")
-#     elif age <= 18:
-#         print("Please pay $7")
-#     else:
-#         print("Please pay $7")
-# else:
-#     print("Sorry, you have to grow taller before you can ride")
+print("Welcome to the rollercoaster")
+height = int(input("What is your height in cm? :"))
+bill = 0
+if height >= 80:
+    print("You can ride the rollercoaster")
+    age = int(input("What is your age? :"))
+    if age < 12:
+        bill = 5
+        print("Child ticket are $5")
+    elif age <= 18:
+        bill = 7
+        print("Youth ticket are $7")
+    else:
+        bill = 12
+        print("Adult ticket are $12")
+    want_photo = input("Do you want a photo taken? Y or N :")
+    if want_photo == "Y":
+        bill = bill + 3
+        print(f"Your final bill is ${bill}")
+    else:
+        bill
+        print(f"Your final bill is ${bill}")
+else:
+    print("Sorry, you have to grow taller before you can ride")
 
 
 
@@ -50,14 +60,14 @@
 
 # Exercise -3****************************************************************
 
-year = int(input("Which year do you want to check?"))
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print(f"{year} is a leap year")
-        else:
-            print(f"{year} is not a leap year")
-    else:
-        print(f"{year} is a leap year")
-else:
-    print(f"{year} is not a leap year")
+# year = int(input("Which year do you want to check?"))
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print(f"{year} is a leap year")
+#         else:
+#             print(f"{year} is not a leap year")
+#     else:
+#         print(f"{year} is a leap year")
+# else:
+#     print(f"{year} is not a leap year")
