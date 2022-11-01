b9f2cb7c4ce172f84be8ff21f8a74e71c577fd1b
mdemon420 <mdeamon4@gmail.com>
2022-11-01 17:38:17 +0600

    first commit

---
+++ Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/index.py
@@ -0,0 +1,40 @@
+# num_char = len(input('what is your name?'))
+# num_str = str(num_char)
+# print("Your name contains " + num_str + " characters")
+
+# tow_digit_number = input('Type tow digit number:')
+# print(tow_digit_number)
+
+# first_digit = tow_digit_number[0]
+# second_digit = tow_digit_number[1]
+# total = int(first_digit) + int(second_digit)
+# print(total)
+
+# print(3*(3+3)/3-3)
+
+# height = input("enter your height m :")
+# weight = input("enter your weight kg :")
+
+# bmi = int(weight) / float(height) ** 2
+# print(bmi)
+
+# age = input("enter your age")
+
+# age_as_int = int(age)
+# year_reaming = 90 - age_as_int
+# days = year_reaming * 365
+# week = year_reaming * 52
+# month = year_reaming * 12
+# print(f"You have {days} days, {week} weeks, {month} months left.")
+
+# print('Welcome to the tip calculator!')
+# bill = float(input('What was the total bill $'))
+# tip = int(input('How much tip would you like to give? 10,12 0r 15?'))
+# people = int(input('How much people to split the bill?'))
+
+# tip_as_percent = tip / 100
+# total_tip = tip_as_percent * bill
+# total_bill = bill + total_tip
+# bill_per_peron = total_bill / people
+# final_amount = round(bill_per_peron, 2)
+# print(f"Each person should pay $ {final_amount}")
