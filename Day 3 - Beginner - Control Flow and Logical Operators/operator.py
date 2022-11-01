140c9f5f935b1637b7cb98ba28305ceaf9ad84a1
mdemon420 <mdeamon4@gmail.com>
2022-11-01 17:45:33 +0600

    add exercise

--- Day 3 - Beginner - Control Flow and Logical Operators/operator.py
+++ Day 3 - Beginner - Control Flow and Logical Operators/operator.py
@@ -1,7 +1,15 @@
-print("Welcome to the rollercoaster")
-height = int(input("What is your height in cm?"))
+# print("Welcome to the rollercoaster")
+# height = int(input("What is your height in cm?"))
 
-if height >= 80:
-    print("You can ride the rollercoaster")
+# if height >= 80:
+#     print("You can ride the rollercoaster")
+# else:
+#     print("Sorry, you have to grow taller before you can ride")
+
+# Exercise
+number = int(input("Which number do you want to choice?"))
+
+if number % 2 == 0:
+    print("This is an even number.")
 else:
-    print("Sorry, you have to grow taller before you can ride")
+    print("This is an odd number.")
