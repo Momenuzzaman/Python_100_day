c124b3d19e5957712065df055f8d052ad69fe0fc
mdemon420 <mdeamon4@gmail.com>
2022-11-02 19:17:07 +0600

    tose

--- Day 4 - Beginner - Randomisation and Python Lists/main.py
+++ Day 4 - Beginner - Randomisation and Python Lists/main.py
@@ -1,10 +1,19 @@
 import random
 
-random_number = random.randint(1, 10)
-print(random_number)
+# random_number = random.randint(1, 10)
+# print(random_number)
 
-random_number_float = random.random()*5
-print(random_number_float)
+# random_number_float = random.random()*5
+# print(random_number_float)
 
-love_score = random.randint(1, 100)
-print(f"You love score is {love_score}")
+# love_score = random.randint(1, 100)
+# print(f"You love score is {love_score}")
+
+# exercise 1********************************
+
+random_number = random.randint(0, 1)
+
+if random_number == 1:
+    print("Head")
+else:
+    print("Tail")
