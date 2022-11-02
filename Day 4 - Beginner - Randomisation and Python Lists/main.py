f678cd3b3912da03ffd46a7999e3ab649ab91f9e
mdemon420 <mdeamon4@gmail.com>
2022-11-02 18:58:47 +0600

    random runber

---
+++ Day 4 - Beginner - Randomisation and Python Lists/main.py
@@ -0,0 +1,10 @@
+import random
+
+random_number = random.randint(1, 10)
+print(random_number)
+
+random_number_float = random.random()*5
+print(random_number_float)
+
+love_score = random.randint(1, 100)
+print(f"You love score is {love_score}")
