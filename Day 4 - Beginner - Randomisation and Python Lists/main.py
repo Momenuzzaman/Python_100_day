af89adbc1e30d330fa5f563f4dcd05bf9aa20c06
mdemon420 <mdeamon4@gmail.com>
2022-11-02 20:16:32 +0600

    random name

--- Day 4 - Beginner - Randomisation and Python Lists/main.py
+++ Day 4 - Beginner - Randomisation and Python Lists/main.py
@@ -11,9 +11,19 @@ import random
 
 # exercise 1********************************
 
-random_number = random.randint(0, 1)
+# random_number = random.randint(0, 1)
 
-if random_number == 1:
-    print("Head")
-else:
-    print("Tail")
+# if random_number == 1:
+#     print("Head")
+# else:
+#     print("Tail")
+
+names_string = input("Give me everybody's names.")
+names = names_string.split(",")
+
+# names_item = len(names)
+# random_name = random.randint(0, names_item-1)
+# bill_pay = names[random_name]
+# print(bill_pay)
+bill_pay = random.choice(names)
+print(bill_pay)
