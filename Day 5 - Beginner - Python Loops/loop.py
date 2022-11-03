5b646fb77361ff3926eb3ce3f786235108dc7f7e
mdemon420 <mdeamon4@gmail.com>
2022-11-03 11:03:31 +0600

    max value

--- Day 5 - Beginner - Python Loops/loop.py
+++ Day 5 - Beginner - Python Loops/loop.py
@@ -2,22 +2,41 @@
 # for fruit in fruits:
 #     print(fruit)
 
-student_heights = input("Input a list of student height :").split()
-for n in range(0, len(student_heights)):
-    student_heights[n] = int(student_heights[n])
+# student_heights = input("Input a list of student height :").split()
+# for n in range(0, len(student_heights)):
+#     student_heights[n] = int(student_heights[n])
 
-total_height = 0
-for height in student_heights:
-    total_height += height
+# 1st logic
+# total_height = 0
+# for height in student_heights:
+#     total_height += height
 
-student_len = 0
-for student in student_heights:
-    student_len += 1
+# student_len = 0
+# for student in student_heights:
+#     student_len += 1
 
-avg = round(total_height / student_len)
-print(avg)
+# avg = round(total_height / student_len)
+# print(avg)
 
+
+# 2nd logic
 # student_height_sum = (sum(student_heights))
 # student_len = len(student_heights)
 # avg = round(student_height_sum / student_len)
 # print(avg)
+
+
+# exercise 2************************************************
+
+student_score = input("Input a list of student scores :").split()
+for n in range(0, len(student_score)):
+    student_score[n] = int(student_score[n])
+
+# max_score = 0
+# for score in student_score:
+#     if score > max_score:
+#         max_score = score
+#         print(max_score)
+
+max_value = max(student_score)
+print(max_value)
