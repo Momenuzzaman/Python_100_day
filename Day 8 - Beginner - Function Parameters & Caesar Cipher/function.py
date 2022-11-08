dfd09d355b03d894dbfe9bfb44e4532753c52468
mdemon420 <mdeamon4@gmail.com>
2022-11-08 22:14:55 +0600

    input with function

---
+++ Day 8 - Beginner - Function Parameters & Caesar Cipher/function.py
@@ -0,0 +1,14 @@
+# def greet():
+#     print('Emon')
+#     print('Liton')
+#     print('Ruhi')
+
+
+# greet()
+
+def my_function(name):
+    print(f'How are you {name}?')
+
+
+my_function('Emon')
+my_function('Liton')
